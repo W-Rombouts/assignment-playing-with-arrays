34e0e4d699a31d14bef3cfefff5f424f9bcdd314
Wouter Rombouts <wouter_rombouts@hotmail.com>
2020-03-26 08:46:55 +0100

    Finished assignment

--- js/script.js
+++ js/script.js
@@ -1,25 +1,16 @@
 let array1 = [300,20,60,100,400,130,270,290,30,450,50];
 
-function sort(toSortList) {
-    if (toSortList.length == 1){
-        console.log(toSortList)
-        return toSortList;
-    }
-    else{
-        //console.log(toSortList)
-        savedNumber= toSortList.pop()
-        //console.log(savedNumber)
-        toSortList = sort(toSortList)
-        console.log(toSortList,savedNumber)
-        if (savedNumber<toSortList[toSortList.length-1]) {
-            toSortList.push(savedNumber)
-        }
-        else{
-            toSortList.unshift(savedNumber)
-            toSortList = sort(toSortList)
-        }
-        return toSortList
-    }
-}
+array1.sort(function(a, b){return b-a});
+console.log(array1)
+array1.pop()
+array1.shift()
 
-console.log(sort(array1))
+console.log("Array1 is nu :"+array1)
+
+let array2 = [1,2,3,4,5,6,7,8,9,10];
+array3 = array2.map((val, i, arr) => {
+    return val * 4;
+    });
+
+console.log("array3 is nu:"+array3)
+
